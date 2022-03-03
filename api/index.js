ccab15f8d951ace6137cf7bd13cec7a8f40536e6
Jaiden Reddy <jaidenreddy@gmail.com>
2022-03-02 19:41:03 -0800

    Add database connection for api

--- api/index.js
+++ api/index.js
@@ -1,12 +1,23 @@
 import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
+import { MongoClient } from "mongodb";
+
+let DATABASE_NAME = "practice_tracker";
 
 const api = express.Router();
+let conn = null;
+let db = null;
+let Users = null;
+let Posts = null;
 
 const initApi = async app => {
   app.set("json spaces", 2);
   app.use("/api", api);
+  conn = await MongoClient.connect("mongodb://localhost");
+  db = conn.db("practice_tracker");
+  Users = db.collection("users");
+  Posts = db.collection("practices");
 };
 
 api.use(bodyParser.json());
